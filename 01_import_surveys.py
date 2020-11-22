cd5be1a48075cf38dae1411d3a0237dcf5bd6ff7
KylieLAnglin <kylie@anglin.com>
2020-11-21 20:27:17 -0500

    import all four surveys

--- 01_import_surveys.py
+++ 01_import_surveys.py
@@ -28,12 +28,36 @@ survey2_path = os.path.join(start.raw_data_path,
 "*EAD During Survey #1 (Reflection after Student Responses)_November 6, 2020_18.18.csv")
 survey2_labels = qualtrics.extract_column_labels(survey2_path)
 survey2 = qualtrics.select_valid_rows(survey2_path)
-survey2 = survey2.loc[((~survey2.Q65.isnull()) | (~survey2.Q66.isnull())) &
-                    (~survey2.Q82.str.startswith('test'))]
 survey2 = qualtrics.drop_meta_data(survey2)
+survey2 = survey2.rename(columns = {'Q65':'hear_text', 'Q66':'hope_hear_text',
+                                    'Q82':'email'})
+survey2 = survey2.loc[((~survey2.hear_text.isnull()) | (~survey2.hope_hear_text.isnull())) &
+                    (survey2.email.str.startswith('test') != True) &
+                    (survey2.email.str.startswith('rhonda.bondie') != True)]
+survey2['survey'] = 'during1'
+
 
 survey3_path = os.path.join(start.raw_data_path, 
 "*EAD During Survey #2 Self-Reflection_November 6, 2020_18.19.csv")
 survey3_labels = qualtrics.extract_column_labels(survey3_path)
 survey3 = qualtrics.select_valid_rows(survey3_path)
 survey3 = qualtrics.drop_meta_data(survey3)
+survey3 = survey3.rename(columns = {'Q83':'hear_text', 'Q84':'hope_hear_text',
+                                    'Q1': 'email'})
+survey3 = survey3.loc[((~survey3.hear_text.isnull()) | (~survey3.hope_hear_text.isnull())) &
+                    (survey3.email.str.startswith('test') != True) &
+                    (survey2.email.str.startswith('rhonda.bondie') != True)]
+survey3['survey'] = 'during2'
+
+
+survey4_path = os.path.join(start.raw_data_path, 
+"*EAD Post Simulation Reflection_November 6, 2020_18.17.csv")
+survey4_labels = qualtrics.extract_column_labels(survey4_path)
+survey4 = qualtrics.select_valid_rows(survey4_path)
+survey4 = qualtrics.drop_meta_data(survey4)
+survey4 = survey4.rename(columns = {'Q1': 'email'})
+survey4 = survey4.loc[
+                    (survey4.email.str.startswith('test') != True) &
+                    (survey2.email.str.startswith('rhonda.bondie') != True)]
+survey4['survey'] = 'post'
+
